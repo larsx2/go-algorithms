dfabf89554d29f0843e185f3b7a00a74aab5a439
Eduardo Urias <windkaiser@gmail.com>
2015-02-12 01:53:07 -0700

    updated: bubble sort alternative more idiomatic

--- src/bubble-sort.go
+++ src/bubble-sort.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// using a 'while true' approach
 func BubbleSort(items []int) []int {
 	swapped := true
 
@@ -19,7 +20,20 @@ func BubbleSort(items []int) []int {
 	return items
 }
 
+// cleaner version
+func BubbleSort2(items []int) []int {
+
+	for i, j := 0, 1; i < len(items)-1; i, j = i+1, j+1 {
+		if items[i] > items[j] {
+			items[i], items[j] = items[j], items[i]
+		}
+	}
+
+	return items
+}
+
 func main() {
 	unorderedArray := []int{2, 5, 0, 3, 1, 12, 123, 13, 12312312, -1}
 	fmt.Println(BubbleSort(unorderedArray))
+	fmt.Println(BubbleSort2(unorderedArray))
 }
